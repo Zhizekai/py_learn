9e7d83be241d60d00bda169246e965ddaa1628ef
zhizekai <1310248516@qq.com>
2021-02-08 14:17:59 +0800

    update: 玫瑰图做完了，准备第六问

--- meisai/meisai3.py
+++ meisai/meisai3.py
@@ -1,32 +1,33 @@
 # %%
 import numpy as np
 import pandas as pd
+
 # %%
 # 同一流派的音乐家
-# full_music_data_sample = pd.read_csv("./full_music_data_sample.csv")
-# # %%
-# influence_man_data = pd.read_csv('./influence_man_data3.csv')
-#
-# # %%
-# pop_rock_influence_man = influence_man_data[influence_man_data["influencer_main_genre"] == "Pop/Rock"]
-# full_music_data_sample["artist_names"] = full_music_data_sample["artist_names"].apply(eval)  # 字符串转换成数组
-#
-#
-# # %%
-#
-# def get_x(x):
-#     print(x[0])
-#     return x[0]
-#
-#
-# full_music_data_sample["artist_names"] = full_music_data_sample["artist_names"].apply(get_x)
-# pop_rock_song = full_music_data_sample[
-#     full_music_data_sample["artist_names"].isin(pop_rock_influence_man["influencer_name"])]
-# # %%
-# pop_rock_song.index = np.arange(len(pop_rock_song))
-# pop_rock_song.to_csv('./pop_rock_song.csv',index=False)
-# # %%
-# pop_rock_song.corr().to_csv('./pop_rock_song_corr.csv')
+full_music_data_sample = pd.read_csv("./full_music_data_sample.csv")
+# %%
+influence_man_data = pd.read_csv('./influence_man_data3.csv')
+
+# %%
+pop_rock_influence_man = influence_man_data[influence_man_data["influencer_main_genre"] == "Pop/Rock"]
+full_music_data_sample["artist_names"] = full_music_data_sample["artist_names"].apply(eval)  # 字符串转换成数组
+
+
+# %%
+
+def get_x(x):
+    print(x[0])
+    return x[0]
+
+
+full_music_data_sample["artist_names"] = full_music_data_sample["artist_names"].apply(get_x)
+pop_rock_song = full_music_data_sample[
+    full_music_data_sample["artist_names"].isin(pop_rock_influence_man["influencer_name"])]
+# %%
+pop_rock_song.index = np.arange(len(pop_rock_song))
+pop_rock_song.to_csv('./pop_rock_song.csv', index=False)
+# %%
+pop_rock_song.corr().to_csv('./pop_rock_song_corr.csv')
 # %%
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
@@ -110,3 +111,51 @@ def get_x(x):
 
 
 full_sample["artist_names"] = full_sample["artist_names"].apply(get_x)
+# %%
+pop_rock_song = pd.read_csv('./pop_rock_song.csv')
+# %%
+pop_rock_song["artist_names"].value_counts().to_csv('./song_nums.csv')
+
+# %%
+# The Kinks   The Beatles
+the_kinks_songs = pop_rock_song[pop_rock_song["artist_names"] == "The Kinks"]
+the_kinks_songs.index = np.arange(len(the_kinks_songs))
+the_kinks_songs = guiyi(the_kinks_songs.loc[:, ['danceability', 'energy', 'valence',
+                                                'tempo', 'loudness', 'mode', 'key', 'acousticness', 'instrumentalness',
+                                                'liveness', 'speechiness', 'duration_ms', 'popularity']])
+
+the_beatles_songs = pop_rock_song[pop_rock_song["artist_names"] == "The Beatles"]
+the_beatles_songs.index = np.arange(len(the_beatles_songs))
+the_beatles_songs = guiyi(the_beatles_songs.loc[:, ['danceability', 'energy', 'valence',
+                                                    'tempo', 'loudness', 'mode', 'key', 'acousticness',
+                                                    'instrumentalness',
+                                                    'liveness', 'speechiness', 'duration_ms', 'popularity']])
+
+# %%
+the_kinks = pd.DataFrame({"tezheng": the_kinks_songs.median().index, "value": the_kinks_songs.median().values})
+
+the_beatles = pd.DataFrame({"tezheng": the_beatles_songs.median().index, "value": the_beatles_songs.median().values})
+
+# %%
+# =======自己设置开始============
+# 标签
+labels = the_kinks["tezheng"].to_list()
+# 数据个数
+dataLenth = the_kinks["tezheng"].shape[0]
+# 数据
+data = the_kinks["value"].to_list()
+# ========自己设置结束============
+
+angles = np.linspace(0, 2 * np.pi, dataLenth, endpoint=False)
+labels = np.concatenate((labels, [labels[0]]))  # 闭合
+data = np.concatenate((data, [data[0]]))  # 闭合
+angles = np.concatenate((angles, [angles[0]]))  # 闭合
+
+fig = plt.figure()
+ax = fig.add_subplot(111, polar=True)  # polar参数！！
+ax.plot(angles, data, 'bo-', linewidth=2)  # 画线
+ax.fill(angles, data, facecolor='r', alpha=0.25)  # 填充
+ax.set_thetagrids(angles * 180 / np.pi, labels, fontproperties="SimHei")
+ax.set_title("the_kinks", va='bottom', fontproperties="SimHei")
+ax.set_rlim(0, 1)
+ax.grid(True)
